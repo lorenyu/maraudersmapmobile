524b08409b5de361198daa2a5a20971a9f7f414c
yuloren <yuloren@57234b71-603d-0410-964b-294494ad4392>
2007-11-03 04:06:40 +0000

    Added example python files and mmm_options python file.

---
+++ MaraudersMapMobile/src/python/mmm_options.py
@@ -0,0 +1,27 @@
+import appuifw, e32
+
+def doNothing():
+    appuifw.note(u"Sorry can't do that!")
+
+def shareLocation():
+    swfLock = e32.Ao_lock()
+    swfLoader = appuifw.Content_handler(swfLock.signal)
+    swfLoader.open("E:\\Others\\App.swf")
+    swfLock.wait()
+    appuifw.note(u"Location shared")
+
+def quit():
+    app_lock.signal()
+
+canvas = appuifw.Canvas()
+appuifw.app.body = canvas
+appuifw.app.exit_key_handler = quit
+appuifw.app.title = u"PhotoEditor"
+appuifw.app.menu = [
+    (u"Mute", doNothing),
+    (u"Loud Speaker", doNothing),
+    (u"Hold", doNothing),
+    (u"Share Location", shareLocation)]
+
+app_lock = e32.Ao_lock()
+app_lock.wait()
