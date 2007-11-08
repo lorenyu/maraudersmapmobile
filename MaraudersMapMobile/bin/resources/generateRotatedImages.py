de7403956cf00e60fb0872eacfb3c003b6f0f9e6
yuloren <yuloren@57234b71-603d-0410-964b-294494ad4392>
2007-11-08 11:26:39 +0000

    updated gates images

--- MaraudersMapMobile/bin/resources/generateRotatedImages.py
+++ MaraudersMapMobile/bin/resources/generateRotatedImages.py
@@ -3,7 +3,7 @@ import Image
 imageFile = 'gates.png'
 numRotations = 16
 
-image = Image.open('gates.png')
+image = Image.open(imageFile)
 degrees = 360 / numRotations
 for i in range(numRotations):
     image.rotate(i * degrees).save('gates_' + ('%03i' % (i * degrees)) + '.jpg')
