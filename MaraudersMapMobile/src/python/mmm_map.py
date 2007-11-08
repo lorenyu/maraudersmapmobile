3644e6a8ba2b692470987aff5296ee1700964459
marcia.lee <marcia.lee@57234b71-603d-0410-964b-294494ad4392>
2007-11-08 12:29:26 +0000

    changed gps coord stuff

--- MaraudersMapMobile/src/python/mmm_map.py
+++ MaraudersMapMobile/src/python/mmm_map.py
@@ -1,16 +1,29 @@
 from appuifw import *
 from graphics import *
+from e32 import *
+
+if (not(in_emulator())):
+    import positioning
+
+def updatePosition(event):
+    positionInfo = event['position']
+    courseInfo = event['course']
+    print 'lat: ', positionInfo['latitude']
+    print 'lon: ', positionInfo['longitude']
 
 class Map:
     def __init__(self):
         try:
             print "Loading map image"
             self.image = Image.open("C:\\Data\\Images\\tressider2-medquality.jpg")
+            self.orig_image = self.image
+            self.zoom = 1
+            self.x = 0
+            self.y = 0
             self.overlay = Image.open("C:\\Data\\Images\\mapui1.jpg")
             self.overlay_mask = Image.new(self.overlay.size, mode = 'L')
-            #self.overlay_mask.load("C:\\Data\\Images\\mapui1_mask2.png")
-            self.overlay_mask.blit(Image.open("C:\\Data\\Images\\mapui1.jpg"))
-            # read this in from text file - can eval(text), and then have a dictionary
+            self.overlay_mask.blit(Image.open("C:\\Data\\Images\\mapui1_mask.jpg"))
+            
             print "Loading map coords"
             f = file(u"C:\\Data\\Others\\tressider2-coords.txt", "r")
             str = ""
@@ -20,6 +33,13 @@ class Map:
             mapCoords = eval(str)		
             self.coords = mapCoords
             print "Map coords: ", self.coords
+            
+            if (not(in_emulator())):
+                positioning.set_requestors([{
+                    "type" : "service",
+                    "format" : "application",
+                    "data"   : "test_app"}])
+                positioning.position(course = 1, callback = updatePosition, interval = 500000)
         except Exception, e:
             print e
         print "Map constructor"
