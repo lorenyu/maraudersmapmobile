268cd1f72ef3a688d69b70584726a8871be47d85
marcia.lee <marcia.lee@57234b71-603d-0410-964b-294494ad4392>
2007-12-05 05:20:01 +0000

    opening new images for zoom instead of resizing, confirming share screen

---
+++ MaraudersMapMobile/src/python/aammm_main.py
@@ -0,0 +1,24 @@
+from appuifw import *
+from e32 import *
+from graphics import *
+from mmm_talkingUI import *
+from mmm_mapUI import *
+from mmm_receivedLocationUI import *
+from mmm_loadMapUI import *
+from mmm_confirmingShareUI import *
+    
+app.redraw = None
+app.keyBindings = None
+
+def redraw(rect):
+    if (app.redraw != None):
+        app.redraw(rect)
+
+app.mapUI = MapUI()
+app.talkingUI = TalkingUI()
+app.receivedLocationUI = ReceivedLocationUI()
+app.loadMapUI = LoadMapUI()
+app.confirmingShareUI = ConfirmingShareUI()
+
+app.body = Canvas(redraw_callback = redraw)
+app.talkingUI.run()
